fe1f363b389c2771ce6e846f0a2029a1c914c89b
ilju <ilju123456@naver.com>
2017-06-21 19:46:14 +0900

    hostinput view 완성~!

---
+++ Yolo/WebContent/js_yolo/host/hostregister.js
@@ -0,0 +1,17 @@
+
+$(function(){
+	
+	$('#imageUpload').change(function(){			
+		readImgUrlAndPreview(this);
+		function readImgUrlAndPreview(input){
+			 if (input.files && input.files[0]) {
+		            var reader = new FileReader();
+		            reader.onload = function (e) {			            	
+		                $('#imagePreview').attr('src', e.target.result);
+						}
+		          };
+		          reader.readAsDataURL(input.files[0]);
+		     }	
+	});
+	
+});
