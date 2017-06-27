b18de4c2df36fea48fe655cf19c6c11f3eb2c3ed
chanjong <ckswhd1128@naver.com>
2017-06-27 20:17:48 +0900

    comShare/shareInput.js 추가수정 dto/WishlisgVo.java , ShareController.java, ShareVO.java, UserVO.java DB와 같은 이름에서 소문자로 수정 (getter setter 수정)

--- Yolo/src/yolo/share/controller/ShareController.java
+++ Yolo/src/yolo/share/controller/ShareController.java
@@ -4,10 +4,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/*
+ * @클래스명: ShareController
+ * 
+ * @version 	1.0 17/06/27
+ * @author		김찬종
+ * @see			yolo.share.controller
+ * @since		JDK1.8
+ */
 @Controller
 @RequestMapping("comShare")
 public class ShareController {
+	/* 멤버필드 */
 	
+	/*
+	 * @메소드명: shareView
+	 * @역활 : url 따라서 페이지 전환
+	 * 
+	 * @param 없음
+	 * @return	String:반환하는 경로
+	 */
 	@RequestMapping("/{url}.share")
 	public String shareView(@PathVariable String url){
 		System.out.println(url);
