7a6c1f1d9f2d731f6381a2a85cf5d531a8b37552
ilju <ilju123456@naver.com>
2017-06-27 21:12:18 +0900

    common-servlet

--- Yolo/src/yolo/host/dao/HostDAO.java
+++ Yolo/src/yolo/host/dao/HostDAO.java
@@ -1,5 +1,7 @@
 package yolo.host.dao;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import yolo.host.dto.HostinfoVO;
@@ -7,8 +9,10 @@ import yolo.host.dto.HostinfoVO;
 @Repository
 public class HostDAO {
 	
+	@Autowired
+	SqlSessionTemplate 
 	
-	public void hostInput(HostinfoVO hvo){
+	public void hostinputok(HostinfoVO hvo){
 		
 	}
 	
--- Yolo/src/yolo/host/service/HostServiceImpl.java
+++ Yolo/src/yolo/host/service/HostServiceImpl.java
@@ -15,8 +15,8 @@ public class HostServiceImpl implements HostService {
 	
 	@Override
 	public void hostinputok(HostinfoVO hvo) {
-		
-		
+		hdao.hostinputok(hvo);
+				
 	}
 	
 	
