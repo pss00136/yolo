3c4add4d4562ae1cb72c2edf57855221a2c9ae3d
LeeYJ <pss00136@gmail.com>
2017-06-28 10:40:00 +0900

    yolo.lecture 주석추가

--- Yolo/src/yolo/lecture/service/LectureService.java
+++ Yolo/src/yolo/lecture/service/LectureService.java
@@ -2,6 +2,22 @@ package yolo.lecture.service;
 
 import yolo.lecture.dto.LectureVO;
 
+/*
+* @인터페이스명: LectureService
+*
+* @version     1.0 17/06/27
+* @author      이윤정
+* @see         yolo.lecture.service
+* @since       JDK1.8
+*/
 public interface LectureService {
-	public void lectureInput( LectureVO vo );
+	
+	/*
+	* @메소드명: lectureInput
+	* @역할: LectureController에서 전달받은 값을 DAO로 전달, 결과값 리턴
+	*
+	* @param   LectureVO:LectureController에서 전달받은 값
+	* @return  int : 결과값
+	*/
+	public int lectureInput( LectureVO vo );
 }
--- Yolo/src/yolo/lecture/service/LectureServiceImpl.java
+++ Yolo/src/yolo/lecture/service/LectureServiceImpl.java
@@ -1,16 +1,21 @@
 package yolo.lecture.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import yolo.lecture.dao.LectureDAO;
 import yolo.lecture.dto.LectureVO;
 
 @Service
 public class LectureServiceImpl implements LectureService {
+	
+	@Autowired
+	LectureDAO dao;
 
 	@Override
-	public void lectureInput( LectureVO vo ) {
-		// TODO Auto-generated method stub
-		
+	public int lectureInput( LectureVO vo ) {
+		int result = dao.lectureInsert(vo);
+		return result;
 	}
 
 }
