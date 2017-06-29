9d29c6dcc1015b7aca40124662b66ae31e69f0ee
ilju <ilju123456@naver.com>
2017-06-29 14:43:25 +0900

    session

--- Yolo/src/yolo/lot/controller/LotController.java
+++ Yolo/src/yolo/lot/controller/LotController.java
@@ -1,5 +1,7 @@
 package yolo.lot.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,7 +39,7 @@ public class LotController {
 	}
 	
 	 @RequestMapping("lot/LotInputSecond.lot")
-		public ModelAndView lotinputfirst(PrivatelotVO priVO, PrivateimageVO primgVO){	    
+		public ModelAndView lotinputfirst(PrivatelotVO priVO, PrivateimageVO primgVO, HttpSession session ){	    
 		    service.lotinputfirst(priVO,primgVO);
 		    ModelAndView mv = new ModelAndView();
 		    mv.setViewName("/lot/LotInputSecond");
